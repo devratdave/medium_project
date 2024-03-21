eeed32c14a673176bfd6c634feec0970f3a17401
Devrat Dave <devratdave@Devrats-MacBook-Air.local>
2024-03-21 15:31:12 +0530

    added one route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signin />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path={`/blog/:id`} element={<Blog />} />
         <Route path={`/editblog/:id`} element={<EditUserBlog />} />
